1ce37e5c0dd633f4b59b882fa214c6b4b3fa3d3f
chorsey <caylahorsey@gmail.com>
2024-02-23 11:56:03 -0500

    Re-styled header, added 2nd vector squiggle

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
 */
 
+const parentpic = document.getElementsByClassName('click');
+
+click.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
